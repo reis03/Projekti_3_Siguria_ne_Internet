db2be8cf20e97191407f882305024209fa66cd55
Rigoni <osmanajrigon3@gmail.com>
2024-05-19 15:00:00 +0200

    feature/E1-T5: Connected the html templeates and the back logic

--- two_factor_auth.py
+++ two_factor_auth.py
@@ -1,17 +1,11 @@
 import pyotp
 
+
 class TwoFactorAuth:
-    @staticmethod
-    def generate_otp(secret_key):
-        totp = pyotp.TOTP(secret_key)
-        return totp.now()
+    def generate_qr_code(self, account_name, secret_key):
+        otp_uri = pyotp.totp.TOTP(secret_key).provisioning_uri(name=account_name, issuer_name="Super app")
+        return otp_uri
 
-    @staticmethod
-    def generate_qr_code(account_name, secret_key):
-        totp = pyotp.TOTP(secret_key)
-        return totp.provisioning_uri(name=account_name, issuer_name="YourApp")
-
-    @staticmethod
-    def verify_otp(secret_key, otp):
+    def verify_otp(self, secret_key, otp):
         totp = pyotp.TOTP(secret_key)
         return totp.verify(otp)
